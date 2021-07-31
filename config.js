bd8040b51d3288e7acf735d1bd5865a96a8831ef
Unknown <achrafatauil@hotmail.com>
2021-07-31 15:33:19 +0200

    add config file

---
+++ config.js
@@ -0,0 +1,13 @@
+const SESSIONS_GRAPH = process.env.SESSIONS_GRAPH || "http://mu.semte.ch/graphs/sessions";
+const ACCOUNTS_GRAPH = process.env.ACCOUNTS_GRAPH || "http://mu.semte.ch/graphs/accounts";
+const HASH_STRENGTH = process.env.HASH_STRENGTH || 12;
+
+if(typeof HASH_STRENGTH != "number") {
+  throw "HASH_STRENGTH environment variable is not of type 'number'"
+}
+
+export {
+  SESSIONS_GRAPH,
+  ACCOUNTS_GRAPH,
+  HASH_STRENGTH
+}
