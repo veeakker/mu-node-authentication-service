50ded2d959b43210c2be4d827505345193fd3e69
Unknown <achrafatauil@hotmail.com>
2021-07-31 21:34:35 +0200

    add missing semi-colons

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ const ACCOUNTS_GRAPH = process.env.ACCOUNTS_GRAPH || "http://mu.semte.ch/graphs/
 const HASH_STRENGTH = process.env.HASH_STRENGTH || 12;
 
 if(typeof HASH_STRENGTH != "number") {
-  throw "HASH_STRENGTH environment variable is not of type 'number'"
+  throw "HASH_STRENGTH environment variable is not of type 'number'";
 }
 
 export {
   SESSIONS_GRAPH,
   ACCOUNTS_GRAPH,
   HASH_STRENGTH
-}
+};
