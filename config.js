bfbef3c97a224e296ff07ff4f2a7f89431a201c7
Unknown <achrafatauil@hotmail.com>
2021-10-24 13:05:13 +0200

    add config vars for account status

--- config.js
+++ config.js
@@ -6,8 +6,13 @@ if(typeof HASH_STRENGTH != "number") {
   throw "HASH_STRENGTH environment variable is not of type 'number'";
 }
 
+const ACCOUNT_ACTIVE_STATUS = 'http://mu.semte.ch/vocabularies/account/status/active';
+const ACCOUNT_INACTIVE_STATUS = 'http://mu.semte.ch/vocabularies/account/status/inactive';
+
 export {
   SESSIONS_GRAPH,
   ACCOUNTS_GRAPH,
-  HASH_STRENGTH
+  HASH_STRENGTH,
+  ACCOUNT_ACTIVE_STATUS,
+  ACCOUNT_INACTIVE_STATUS
 };
